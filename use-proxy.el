fd01061ba345c15aef7789f87609f87932ce3096
Ray Wang <ray.hackmylife@gmail.com>
2020-08-15 18:32:30 +0800

    follow checkdoc

--- use-proxy.el
+++ use-proxy.el
@@ -1,7 +1,31 @@
-;;; -*- lexical-binding: t; -*-
-;;; use-proxy.el --- Use proxy globally or limiting to single s-expression.
+;;; use-proxy.el --- An easy way to enable/disable proxies in Emacs, and even limited to s-expressions. -*- lexical-binding: t; -*-
 
-;;; Copyright ()
+;;; Copyright (C) 2020-2020 Ray Wang
+
+;;; Author: Ray Wang <ray.hackmylife@gmail.com>
+;;; Keywords: proxy, emacs lisp, toggle
+;;; URL: https://github.com/blueabys/use-proxy
+
+;; This file is not part of GNU Emacs.
+
+;; This file is free software: you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This file is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this file.  If not, see <http://www.gnu.org/licenses/>.
+
+
+;;; Commentary:
+;; With `use-proxy' you can easily enable/disable proxies per protocol in you Emacs.
+
+;;; Code:
 (require 'exec-path-from-shell)
 
 (defgroup use-proxy nil
@@ -14,13 +38,14 @@
     (exec-path-from-shell-copy-env env)))
 
 (defcustom use-proxy-http-proxy (getenv "HTTP_PROXY")
-  "HTTP proxy in HOST:PORT format, default is the value of $HTTP_PROXY"
+  "HTTP proxy in HOST:PORT format, default is the value of $HTTP_PROXY."
   :type '(string)
   :group 'use-proxy)
 
 (defcustom use-proxy-https-proxy (or (getenv "HTTPS_PROXY")
                                      (getenv "HTTP_PROXY"))
-  "HTTPS proxy in HOST:PORT format. If not set, it will first try to
+  "HTTPS proxy in HOST:PORT format.
+If not set, it will first try to
 use the value of $HTTPS_PROXY, and then $HTTP_PROXY"
   :type '(string)
   :group 'use-proxy)
@@ -28,8 +53,7 @@ use the value of $HTTPS_PROXY, and then $HTTP_PROXY"
 (defcustom use-proxy-no-proxy (or (getenv "NO_PROXY")
                                   "^\\(localhost\\|10\\..*\\|192\\.168\\..*\\)")
   "Regular expression described hosts you don't want to access through a proxy.
- If not set, it will first try to use the value of $NO_PROXY,
- and then \"^\\(localhost\\|10\\..*\\|192\\.168\\..*\\)\""
+If not set, it will first try to use the value of $NO_PROXY, and then\"^\\(localhost\\|10\\..*\\|192\\.168\\..*\\)\""
   :type '(string)
   :group 'use-proxy)
 
@@ -51,18 +75,19 @@ use the value of $HTTPS_PROXY, and then $HTTP_PROXY"
   :global t)
 
 (defun use-proxy--trim-proxy-address (address)
-  "Trim proxy address from <scheme>://<host>:<port> into <host>:<port>
- because the former may lead name resolving errors. Default bound to [C-c -C-p]"
+  "Trim proxy ADDRESS from '<scheme>://<host>:<port>' into '<host>:<port>'.
+Because the former may lead name resolving errors."
   (car (last (split-string address "//"))))
 
 (defun use-proxy--get-proxy-by-proto (proto)
-  "Get proxy setting by protocol."
+  "Get proxy setting by protocol.
+Argument PROTO protocol which you want to get proxy of."
   (use-proxy--trim-proxy-address
    (symbol-value (intern-soft (format "use-proxy-%s-proxy" proto)))))
 
 ;;;###autoload
 (defun use-proxy-toggle-proxies-global ()
-  "Toggle if use proxies globally by set/unset no_proxy key in `url-proxy-services'."
+  "Toggle proxies globally by set/unset no_proxy key in `url-proxy-services'."
   (interactive)
   (let ((no-proxy use-proxy-no-proxy))
     (if (eq nil (assoc "no_proxy" url-proxy-services))
@@ -72,9 +97,11 @@ use the value of $HTTPS_PROXY, and then $HTTP_PROXY"
 
 ;;;###autoload
 (defun use-proxy-toggle-proto-proxy (proto)
-  "Toggle proxy on/off. You can switch proxy per protocol,
-and proxy status will show on mode-line. This function will set/unset
-`url-proxy-services' to enable/disable proxies."
+  "Toggle proxy on/off.
+You can switch proxy per protocol,
+and proxy status will show on mode-line.
+ This function will set/unset `url-proxy-services' to enable/disable proxies.
+Argument PROTO protocol which you want to enable/disable proxy for."
   (interactive "P")
   (let* ((proto (completing-read
                  "Switch proxy for: "
@@ -88,8 +115,10 @@ and proxy status will show on mode-line. This function will set/unset
 
 ;;;###autoload
 (defmacro use-proxy-with-custom-proxies (protos &rest body)
-  "Use proxies on a group of S-expressions. This function respects `use-proxy-<protocol>-proxy'
-variables and provide a local `url-proxy-services' to argument `body'."
+  "Use proxies on a group of S-expressions.
+This function respects `use-proxy-<protocol>-proxy' variables,
+and provide a local `url-proxy-services' to argument `BODY'.
+Argument PROTOS protocol list such as '(\"http\" \"https\")."
   `(let ((url-proxy-services
           (mapcar (lambda (proto)
                     (cons proto (use-proxy--get-proxy-by-protocol proto)))
@@ -98,9 +127,11 @@ variables and provide a local `url-proxy-services' to argument `body'."
 
 ;;;###autoload
 (defmacro use-proxy-with-specified-proxies (protos-assoc &rest body)
-  "Use proxies on a group of S-expressions. This function doesn't respect your custom
-`use-proxy-<protocol>-proxy' variables. It provides a local `url-proxy-services'
- to argument `body'."
+  "Use proxies on a group of S-expressions.
+This function doesn't respect custom `use-proxy-<protocol>-proxy' variables.
+It provides a local `url-proxy-services' to argument `BODY'.
+Argument PROTOS-ASSOC protocol association list in the form of
+'((\"http\" . \"localhost:1234\") (\"https\" . \"localhost:2345\"))."
   `(let ((url-proxy-services ,protos-assoc))
      ,@body))
 
