40fa372fc2404e50bf59b268711b2e76598f4eb5
Ray Wang <ray.hackmylife@gmail.com>
2020-08-16 15:18:16 +0800

    fix links

--- use-proxy.el
+++ use-proxy.el
@@ -6,7 +6,7 @@
 ;; Version: 1.0.0
 ;; Package-Requires: ((exec-path-from-shell "20200526.324"))
 ;; Keywords: proxy, environment, toggle
-;; URL: https://github.com/blueabys/use-proxy
+;; URL: https://github.com/blueabysm/use-proxy
 
 ;; This file is not part of GNU Emacs.
 
